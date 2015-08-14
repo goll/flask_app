6724d114cf6587b66b69176195609c686721c128
goll <adrian.goll@gmail.com>
2015-08-14 13:47:11 +0200

    Add debug mode logging

--- provisioning/files/dr.py
+++ provisioning/files/dr.py
@@ -18,13 +18,15 @@ users = db['users']
 def add_entry():
     """Reads JSON entries, checks their md5checksums and stores only valid entries in db, discards the rest."""
     all_entries = True
-    entries = request.get_json(force=True)
+    entries = request.get_json(force=True, silent=True)
+    app.logger.debug('[add_entry] Entries: %s' % request.data)
     if type(entries) == list:
         for entry in entries[:]:
             uid, name, date, md5checksum = (entry.get('uid'), entry.get('name'), entry.get('date'), entry.get('md5checksum'))
             checksum_string = '{"date": "%s", "uid": "%s", "name": "%s"}' % (date, uid, name)
             checksum = md5(checksum_string).hexdigest()
             if checksum != md5checksum:
+                app.logger.debug('[add_entry] Checksum mismatch: %s' % entry)
                 entries.remove(entry)
                 all_entries = False
             else:
@@ -32,6 +34,7 @@ def add_entry():
                     entry['uid'] = int(uid)
                     entry['date'] = parser.parse(date if date else None)
                 except (ValueError, TypeError, AttributeError, KeyError):
+                    app.logger.debug('[add_entry] Invalid format: %s' % entry)
                     entries.remove(entry)
                     all_entries = False
         if entries:
@@ -43,18 +46,21 @@ def add_entry():
         else:
             return jsonify(success='false', message='No entries added (checksum mismatch or invalid format)')
     else:
+        app.logger.error('[add_entry] Invalid input: %s' % request.data)
         return jsonify(success='false', message='Invalid input'), 400
 
 
 @app.route('/show', methods=['GET'])
 def show_entry():
     """Shows the number of occurrences of an uid on a given date."""
+    app.logger.debug('[show_entry] Parameters: ?%s' % request.query_string)
     counter = 0
     try:
         uid = int(request.args.get('uid'))
         date = request.args.get('date')
         date = parser.parse(date if date else None)
     except (ValueError, TypeError, AttributeError):
+        app.logger.error('[show_entry] Invalid parameters: %s' % request.args)
         return jsonify(success='false', message='Invalid parameters'), 400
     if uid and date:
         start = datetime(date.year, date.month, date.day, 0, 0, 0)
@@ -63,6 +69,7 @@ def show_entry():
             counter += 1
         return jsonify(success='true', uid=uid, date=request.args.get('date'), occurrences=counter)
     else:
+        app.logger.error('[show_entry] Invalid parameters: %s' % request.args)
         return jsonify(success='false', message='Invalid parameters'), 400
 
 if __name__ == '__main__':
